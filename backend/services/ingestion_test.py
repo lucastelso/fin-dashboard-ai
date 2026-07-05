8bb365a6b6f49489ab2a2858559153825d60466c
Lucas <lucastellucciso@gmail.com>
2026-07-05 17:52:56 -0300

    teste rodando e funcional/home/berserker/Documentos/projetos/fin-dashboard-ai/.venv/bin/python

--- backend/services/ingestion_test.py
+++ backend/services/ingestion_test.py
@@ -11,7 +11,7 @@ async def main():
     tickers = ["BBAS3.SA", "PETR4.SA", "VALE3.SA", "BOVA11.SA", "IVVB11.SA"]
     
     # Conversão de datas para Unix Timestamp (exigência da API do Yahoo)
-    start_dt = datetime(2026, 6, 1, tzinfo=timezone.utc)
+    start_dt = datetime(2026, 1, 1, tzinfo=timezone.utc)
     end_dt =datetime.now(tz=timezone.utc)
     
     start_ts = int(start_dt.timestamp())
@@ -45,10 +45,18 @@ async def main():
             "low": pl.Float32,
             "close": pl.Float32,
             "volume": pl.Int64
-        })
+            })
 
-        logger.info("\n=== Amostra dos Dados (API Direta) ===")
-        logger.info(f"\n{master_df.head()}")
+        
+        logger.info("\n\n=============== Amostra dos Dados (API Direta) ================")
+        print(master_df.select([
+            pl.col("ticker").alias("ativo"),
+            pl.col("date").alias("data"),
+            pl.col("open").alias("abertura").round(2),
+            pl.col("close").alias("fechamento").round(2),
+            pl.col("volume")
+        ]).sort("data"))
+
         
     else:
         logger.warning("Nenhum dado pôde ser ingerido da API direta.")
