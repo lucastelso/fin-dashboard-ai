dc926116c7af689024c59cde8e3267d744e72f6f
Lucas <lucastellucciso@gmail.com>
2026-07-09 10:23:56 -0300

    aplicação viva e funcionando! inciando criação dos indicadores

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,44 @@
 # backend/main.py
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, APIRouter
 import concurrent.futures
 import multiprocessing
-import asyncio
 
 from core.logger import logger
-# from api.routers import market  # Descomentaremos no próximo passo
+# from api.routers import market 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info("Inicializando Dashboard Financeiro - API...")
-    
     workers = max(1, multiprocessing.cpu_count() - 2)
     app.state.process_pool = concurrent.futures.ProcessPoolExecutor(max_workers=workers)
-    logger.info(f"ProcessPool instanciado com {workers} processos paralelos.")
-    
     yield
-    
-    logger.info("Iniciando desligamento do ProcessPool. Aguardando workers finalizarem...")
+    logger.info("Desligando ProcessPool...")
     app.state.process_pool.shutdown(wait=True, cancel_futures=True)
 
 app = FastAPI(
-    title="Dashboard Financeiro - Analytics de Ativos",
-    description="API de análise quantitativa e qualitativa (IA) de ativos de mercado",
+    title="Dashboard Financeiro Inteligente",
     lifespan=lifespan
 )
 
-# Adicionamos o prefixo /api para casar com a regra de roteamento do Nginx
-@app.get("/api/health", tags=["Infraestrutura"])
-async def health_check():
-    return {"status": "operacional"}
+# 1. Criamos o Router Global que vai abraçar TUDO da API.
+api_router = APIRouter(prefix="/api-financeira")
 
-# Quando formos usar os routers separados, faremos assim:
-# app.include_router(market.router, prefix="/api/v1/market", tags=["Market Data"])
+# 2. As rotas agora são penduradas no api_router, e não mais no app diretamente
+@api_router.get("/health", tags=["Infraestrutura"])
+async def health_check():
+    return {
+        "status": "operacional, banco de dados isolado", 
+        "versao": "alpha"
+        }
+
+@api_router.get("/dashboard", tags=["Analytics"])
+async def get_dashboard_metrics():
+    # Simulando um payload
+    return {
+        "kpi_mensal_inventado": 14500.50, 
+        "risco_sistemico": "baixo",
+        "ativos_em_alta": ["AAPL", "GOOGL", "AMZN"]}
+
+# 3. super-árvore de rotas na aplicação principal
+app.include_router(api_router)
