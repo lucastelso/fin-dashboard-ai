6d438adabcc6aa144bef9ad0e462ebc9e14b7a62
Lucas <lucastellucciso@gmail.com>
2026-07-09 10:38:37 -0300

    alterado o fetch_yahoo para coletar de hora em hora

--- backend/services/fetch_yahoo.py
+++ backend/services/fetch_yahoo.py
@@ -31,7 +31,7 @@ async def fetch_yahoo_json_async(
     params = {
         "period1": start_timestamp,
         "period2": end_timestamp,
-        "interval": "1d",
+        "interval": "1h",
         "events": "history"
     }
     
